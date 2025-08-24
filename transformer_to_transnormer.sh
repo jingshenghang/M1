4a023a6022292c5c770a231b489c21d6bf2a13d7
jingshenghang <jingshenghang@163.com>
2025-08-24 14:51:36 +0800

    modified sh

--- transformer_to_transnormer.sh
+++ transformer_to_transnormer.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python  -m pdb transformer_to_transnormer.py --model_name /home/data/models/Qwen3-30B-A3B --checkpoint_folder  /home/data/models/Qwen3-30B-A3B-transformer_to_transnormer --expand 2 --attn_layers "0,2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36,38,40,42,44,46,47"
+python  transformer_to_transnormer.py --model_name /home/data/models/Qwen3-30B-A3B --checkpoint_folder  /home/data/models/Qwen3-30B-A3B-transnormer --expand 2 --attn_layers "0,2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36,38,40,42,44,46,47"
